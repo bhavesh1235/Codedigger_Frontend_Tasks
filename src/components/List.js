c1e5e68834576c534cd9ec8ffbb8d806b36ec290
Bhavesh Lokre <55055550+bhavesh1235@users.noreply.github.com>
2021-09-09 23:47:15 +0530

    Updated Profile View

--- src/components/List.js
+++ src/components/List.js
@@ -13,31 +13,29 @@ const List = ({
   about_mentor
 }) => {
   return (
-    <div className="col-md-4 animated fadeIn">
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">
-            {`Name: ${name}`}
-            <span
-              style={{ marginLeft: "50px" }}
-            >{`Username: ${username}`}</span>
-          </h5>
-          <p className="card-text">
-            {`Email: ${email}`}
-            <br />
-          </p>
-          <p className="card-text">
-            {`Codeforces: ${codeforces}`}
-            <br />
-          </p>
-          <p className="card-text">
-            {`Codechef: ${codechef}`}
-            <br />
-          </p>
-          <p className="card-text">
-            {`Spoj: ${spoj}`}
-            <br />
-          </p>
+    <div className="card-container">
+      <header>
+        <h1 className="bold-text">
+          {name} <span className="normal-text">{username}</span>
+        </h1>
+      </header>
+      <h2 className="normal-text">{`Email: ${email}`}</h2>
+      <div className="search-container">
+        <div className="codechef">
+          <h1 className="bold-text">{codechef}</h1>
+          <h2 className="smaller-text">Codechef</h2>
+        </div>
+        <div className="codeforces">
+          <h1 className="bold-text">{codeforces}</h1>
+          <h2 className="smaller-text">Codeforces</h2>
+        </div>
+        <div className="atcoder">
+          <h1 className="bold-text">{atcoder}</h1>
+          <h2 className="smaller-text">Atcoder</h2>
+        </div>
+        <div className="spoj">
+          <h1 className="bold-text">{spoj}</h1>
+          <h2 className="smaller-text">Spoj</h2>
         </div>
       </div>
     </div>
